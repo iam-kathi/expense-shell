4d05dd3f38e6bdd90bcf59d94c28e2d3065a084b
barath <bharathkumarkathi224@gmail.com>
2025-09-26 12:10:18 +0530

    MYSQL AUTOMATE USING SHELL

--- mysql.sh
+++ mysql.sh
@@ -43,6 +43,13 @@ VALIDATE $? "Enabled MYSQL sserver"
 systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "Strarted MYSQL server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
-VALIDATE $? "Setting up root password"
+mysql -h mysql.daws81s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting UP root password"
+else
+    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
+fi
 
